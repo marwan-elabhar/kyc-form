6fbd960f7f3e5511ef4fa959fcf44353c665720b
MarwanElAbhar <marwan@aklat.io>
2025-08-22 16:23:38 +0300

    handle form state and submission

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -28,35 +28,52 @@ const questions = [
 ]
 
 import { fieldsMapper } from '../components/fields/mapper';
+import { useEffect, useState } from 'react';
 
 
 export default function Form() {
+    const [formData, setFormData] = useState({});
+
+    useEffect(() => {
+        const initialData = {};
+        questions.forEach(q => {
+            initialData[q.id] = q.type === 'multi_choice' ? [] : '';
+        })
+        setFormData(initialData);
+
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form Data Submitted:', formData);
+    }
+
+
     return (
         <div className="form-container">
-            <h1>Form Page</h1>;
-            {questions.map((question) => {
-                const { id, label, type, options, required, min, max } = question
-                const FieldComponent = fieldsMapper[type];
-                if (!FieldComponent) {
-                    return <div key={id}>Unsupported field type: {type}</div>;
-                }
-                return (
-                    <FieldComponent
-                        key={id}
-                        label={label}
-                        required={required}
-                        options={options}
-                        min={min}
-                        max={max}
-                        value={question.value || ''}
-                        onChange={(value) => {
-                            question.value = value;
-                            console.log(`Updated ${id} to:`, value);
-                        }}
-                    />
-                );
-            })}
-            <button type="submit">Submit</button>
+            <h1>Form Page</h1>
+            <form onSubmit={handleSubmit}>
+                {questions.map((question) => {
+                    const { id, label, type, options, required, min, max } = question
+                    const FieldComponent = fieldsMapper[type];
+                    if (!FieldComponent) {
+                        return <div key={id}>Unsupported field type: {type}</div>;
+                    }
+                    return (
+                        <FieldComponent
+                            key={id}
+                            label={label}
+                            required={required}
+                            options={options}
+                            min={min}
+                            max={max}
+                            value={formData[id]}
+                            onChange={(value) => setFormData({ ...formData, [id]: value })}
+                        />
+                    );
+                })}
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
