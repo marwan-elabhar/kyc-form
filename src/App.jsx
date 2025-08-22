4d4132b77988f2ef08ef04bb4182b6dd22cc6420
MarwanElAbhar <marwan@aklat.io>
2025-08-22 16:11:23 +0300

    import Form component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+import Form from './pages/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Form />
     </>
   )
 }
